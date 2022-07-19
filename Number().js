751f17900ac884f8ec3cc22bca6ff5daf9156fd6
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-19 16:21:18 +0500

    Create Number().js

---
+++ Number().js
@@ -0,0 +1,15 @@
+    /*
+                    NUmber Method() in javascript.
+
+                    whats is number method() in javascript?
+                    number method() is used to perform mathematical operation on number.
+    */
+
+                        var a = "100";
+                        var b = Number(a);
+                        document.write(b + 20);
+                        document.write("<br>");
+
+                        var c  = true;
+                        var d = Number(c);
+                        document.write(d);

e987a6a3714651e51d1c414ddf7aaea13cd8a4d3
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-19 16:29:03 +0500

    Create parseInt()&parseFloat().js

---
+++ parseInt()&parseFloat().js
@@ -0,0 +1,15 @@
+       /*
+                          parseInt(); and parseFloat();
+                            parseInt() is used to convert the string into number.
+                            parseFloat() is used to convert the string into number.
+
+            */
+
+                           var a = "3.142 is the value of pie";
+                           var b = parseInt(a);
+                           document.write(b + "<br>");
+
+
+                           var c = "3.142 is the value of pie";
+                           var d = parseFloat(c);
+                           document.write(d);

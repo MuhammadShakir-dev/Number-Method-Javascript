1e0a87f9be1625c5c8e024333211cb18702d1126
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-19 16:43:08 +0500

    Create toFixed()&toPrecision.js

---
+++ toFixed()&toPrecision.js
@@ -0,0 +1,18 @@
+  /*
+                  toFixed(): and toPrecision();
+                    toFixed() is used to convert the number into string.
+                    toPrecision() is used to convert the number into string.
+                    
+               
+        
+
+
+
+    */
+                    var a = 3.142;
+                    var b = a.toFixed(2);
+                    document.write(b + "<br>");
+                    
+                    var c = 3.142;
+                    var d = c.toPrecision(2);
+                    document.write(d);
